12d66105f625cbe25156d435393a1f2ee3f8376e
NoHOPE <240096962+NoHOPEC@users.noreply.github.com>
2026-09-17 20:36:11 +0530

    Await stream_markup function calls for button creation

--- ShrutixMusic/utils/stream/stream.py
+++ ShrutixMusic/utils/stream/stream.py
@@ -1,3 +1,4 @@
+# ShrutixMusic/utils/stream/stream.py
 import os
 from random import randint
 from typing import Union
@@ -99,7 +100,7 @@ async def stream(
                     forceplay=forceplay,
                 )
                 img = await get_thumb(vidid)
-                button = stream_markup(_, chat_id)
+                button = await stream_markup(_, chat_id)
                 run = await nand.send_photo(
                     original_chat_id,
                     photo=img,
@@ -185,7 +186,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = await stream_markup(_, chat_id)
             run = await nand.send_photo(
                 original_chat_id,
                 photo=img,
@@ -238,7 +239,7 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = await stream_markup(_, chat_id)
             run = await nand.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
@@ -292,7 +293,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = stream_markup(_, chat_id)
+            button = await stream_markup(_, chat_id)
             run = await nand.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
@@ -353,7 +354,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = await stream_markup(_, chat_id)
             run = await nand.send_photo(
                 original_chat_id,
                 photo=img,
@@ -408,7 +409,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = await stream_markup(_, chat_id)
             run = await nand.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
